f511f3ad079fcb44940816f2a46578bb4992da3c
LZ <laraezegart@gmail.com>
2020-10-31 19:58:44 -0700

    added times to timeblocks

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+$(document).ready(function() {
+    const currentDate = moment().format('MMMM Do YYYY, H:mm')
+    console.log(currentDate)
+    $("#current-date").text(currentDate)
 
-const currentDate = moment().format('MMMM Do YYYY, h:mm')
-console.log(currentDate)
-$("#current-date").text(currentDate)
+    const times = [9, 10, 11, 12, 13, 14, 15, 16]
 
-$("form").on("submit", function(e) {
-    e.preventDefault()
-    console.log(e.target)
+    times.forEach(time => {
+        const timeCheck = window.localStorage.getItem(time)
+        
+        const currentHour = moment().hour()
+
+        console.log(currentHour)
+        console.log(time)
+
+
+    })
+
+    $("form").on("submit", function(e) {
+        e.preventDefault()
+        const time = e.target.querySelector("input").getAttribute("id")
+        const text = e.target.querySelector("input").value
+
+        window.localStorage.setItem(time, text)
+})
 })
