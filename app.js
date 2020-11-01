95ea067992811d501f97df5ce29efb5db1c0f105
LZ <laraezegart@gmail.com>
2020-10-31 20:00:29 -0700

    created conditionals for color and changeability of timeblocks

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ $(document).ready(function() {
         console.log(currentHour)
         console.log(time)
 
+        if (currentHour > time) {
+            $(`#${time}`).addClass("bg-secondary  text-light")
+            $(`#${time}`).attr("disabled", true)
+        } else if (currentHour === time) {
+            $(`#${time}`).addClass("bg-danger  text-light")
+        } else {
+            $(`#${time}`).addClass("bg-info  text-light")
+        }
 
+        if (timeCheck === null) {
+            window.localStorage.setItem(time, "")
+        } else if (timeCheck.length > 0) {
+            $(`#${time}`).attr("value", window.localStorage.getItem(time))
+        }
     })
 
     $("form").on("submit", function(e) {
